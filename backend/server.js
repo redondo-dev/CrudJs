d3bf8388970a42ca59a73f0e12d5c8f8680df9fc
redondo <riad.reda.fethi@gmail.com>
2024-03-06 18:58:31 +0100

    crud complet avec node js express et MongoDB

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv =require ("dotenv").config();
+const port = 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/post", require("./routes/post.routes"));
+
+app.listen(port, () => console.log("le serveur est connecté " + port));
